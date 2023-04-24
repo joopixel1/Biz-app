7cb960997f20bccfabbfc8637346803a3a1bb469
aro901362 <ayoarowolo12@gmai.com>
2023-04-24 01:03:50 -0500

    ays first commit

--- header.js
+++ header.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-import style from './Header.module.css'; // assuming you have a Header.css file for styles
+import style from "./Header.module.css"; // assuming you have a Header.css file for styles
 
 function Header() {
-    return (
-      <header className={style.header}>
-        <h1>Business Name Generator</h1>
-        <nav>
-          <ul className={style.header_nav_ul}>
-            <li className={style.header_nav_li}><Link className={style.header_nav_li}href="/">Home</Link></li>
-            <li className={style.header_nav_li}><Link className={style.header_nav_li}href="/login">Login</Link></li>
-            <li className={style.header_nav_li}><Link className={style.header_nav_li}href="/signup">Signup</Link></li>
-            <li className={style.header_nav_li}><Link className={style.header_nav_li}href="#">Menu</Link></li>
-            <li className={style.header_nav_li}><Link className={style.header_nav_li}href="#">Cart</Link></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  export default Header;
-  
+  return (
+    <header className={style.header}>
+      <h1>Business Name Generate</h1>
+      <nav>
+        <ul className={style.header_nav_ul}>
+          <li className={style.header_nav_li}>
+            <Link className={style.header_nav_li} href="/">
+              Home
+            </Link>
+          </li>
+          <li className={style.header_nav_li}>
+            <Link className={style.header_nav_li} href="/login">
+              Login
+            </Link>
+          </li>
+          <li className={style.header_nav_li}>
+            <Link className={style.header_nav_li} href="/signup">
+              Signup
+            </Link>
+          </li>
+          <li className={style.header_nav_li}>
+            <Link className={style.header_nav_li} href="#">
+              Menu
+            </Link>
+          </li>
+          <li className={style.header_nav_li}>
+            <Link className={style.header_nav_li} href="#">
+              Cart
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
